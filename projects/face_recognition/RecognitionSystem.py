c1620f1a69ccd089e135d82dc78750af8c0d0818
Pold Anampa <poldanampa@gmail.com>
2024-11-14 22:28:17 -0500

    draw face mesh

--- projects/face_recognition/RecognitionSystem.py
+++ projects/face_recognition/RecognitionSystem.py
@@ -8,9 +8,49 @@ from PIL import Image, ImageTk
 import imutils
 import math
 
-global name_field, user_field, pass_field
+def biometric_log():
+    global facereg_screen, count, blink, img_info, step, video_cap, lbl_video
+
+    if video_cap is not None:
+        ret, frame = video_cap.read()
+
+        # Resize
+        frame = imutils.resize(frame, width=1280)
+
+        # Change color profile
+        frame_temp = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        if ret==True:
+            # Inference face mesh
+            res = face_mesh.process(frame_temp)
+
+            # Result lists
+            pos_x = []
+            pos_y = []
+            temp_list = []
+
+            if res.multi_face_landmarks:
+                # Extract face mesh
+                for face in res.multi_face_landmarks:
+                    # Draw
+                    mp_draw.draw_landmarks(frame, face, face_mesh_object.FACEMESH_CONTOURS, draw_config, draw_config)
+        
+        # Convert video
+        img = Image.fromarray(frame)
+        img = ImageTk.PhotoImage(image=img)
+
+        # Show video
+        lbl_video.configure(image=img)
+        lbl_video.image = img
+        lbl_video.after(10, biometric_log)
+
+    else:
+        video_cap.release()
 
 def register_user():
+    global name_field, user_field, pass_field, video_cap, lbl_video, facereg_screen
+    
     name, new_user, pwd = name_field.get(), user_field.get(), pass_field.get()
 
     if (len(name) == 0) | (len(new_user) == 0) | (len(pwd) == 0):
@@ -41,7 +81,23 @@ def register_user():
             name_field.delete(0,END)
             user_field.delete(0,END)
             pass_field.delete(0,END)
-    
+
+            # Face Recognition screen
+            facereg_screen = Toplevel(main_screen)
+            facereg_screen.title('BIOMETRIC REGISTER')
+            facereg_screen.geometry('1280x720')
+
+            # Label video
+            lbl_video = Label(facereg_screen)
+            lbl_video.place(x=0, y=0)
+
+            # Video capture
+            video_cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+            video_cap.set(3, 1280)
+            video_cap.set(4, 720)
+
+            biometric_log()
+                
             print('Successful register')
 
 def login_user():
@@ -50,12 +106,37 @@ def login_user():
 users_path = 'projects/face_recognition/databases/users'
 faces_path = 'projects/face_recognition/databases/faces'
 
+# Register variables
+blink = False
+count = 0
+sample = 0
+step = 0
+
+# Offset params
+off_x = 30
+off_y = 20
+
+# Threshold
+th = 0.5
+
+# Draw tool
+mp_draw = mp.solutions.drawing_utils
+draw_config = mp_draw.DrawingSpec(thickness=1, circle_radius=1)
+
+# Face mesh
+face_mesh_object = mp.solutions.face_mesh
+face_mesh = face_mesh_object.FaceMesh(max_num_faces=1, refine_landmarks=True)
+
+# Face detector
+face_object = mp.solutions.face_detection
+face_detector = face_object.FaceDetection(min_detection_confidence=th, model_selection=1)
+
 info = []
 
 # Create screen
-screen = Tk()
-screen.title('FACE RECOGNITION SYSTEM')
-screen.geometry('1280x720')
+main_screen = Tk()
+main_screen.title('FACE RECOGNITION SYSTEM')
+main_screen.geometry('1280x720')
 
 
 # Set background
@@ -65,29 +146,29 @@ background.place(x=0, y=0, relheight=1, relwidth=1)
 
 
 # Create text input fields
-name_field = Entry(screen, width=50)
+name_field = Entry(main_screen, width=50)
 name_field.place(x=110, y=320)
 
-user_field = Entry(screen, width=50)
+user_field = Entry(main_screen, width=50)
 user_field.place(x=110, y=430)
 
-pass_field = Entry(screen, width=50)
+pass_field = Entry(main_screen, width=50)
 pass_field.place(x=110, y=540)
 
-# user_log_field = Entry(screen, width=50)
+# user_log_field = Entry(main_screen, width=50)
 # user_log_field.place(x=750, y=380)
 
-# pass_log_field = Entry(screen, width=50)
+# pass_log_field = Entry(main_screen, width=50)
 # pass_log_field.place(x=750, y=500)
 
 
 # Create buttons
 reg_img = PhotoImage(file='projects/face_recognition/setup/BtSign.png')
-reg_btn = Button(screen, text='Register', image=reg_img, height=40, width=200, command=register_user)
+reg_btn = Button(main_screen, text='Register', image=reg_img, height=40, width=200, command=register_user)
 reg_btn.place(x=300, y=580)
 
 log_img = PhotoImage(file='projects/face_recognition/setup/BtLogin.png')
-log_btn = Button(screen, text='Login', image=log_img, height=40, width=200, command=login_user)
+log_btn = Button(main_screen, text='Login', image=log_img, height=40, width=200, command=login_user)
 log_btn.place(x=900, y=580)
 
-screen.mainloop()
+main_screen.mainloop()
